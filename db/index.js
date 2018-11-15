4712c6e57104385fa252d2a8d7df2cdae4298221
Cole Erickson <pawnstar4@gmail.com>
2018-11-14 20:03:50 -0700

    Mongo error kills server

--- db/index.js
+++ db/index.js
@@ -3,6 +3,10 @@ const user = require('./user')
 
 const connection = mongoose.createConnection(process.env.DB || 'mongodb://localhost/timbr')
 connection.on('open', ()=>console.log('Mongo connected'))
+connection.on('error', err=>{
+  console.error(err)
+  process.exit(1)
+})
 
 const db = {
   user: connection.model('User', user)
