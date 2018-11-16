70beb895515e5e85e1d07d2135960e9699bbc051
Cole Erickson <pawnstar4@gmail.com>
2018-11-16 01:29:59 -0700

    Landing page recent profiles

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = require('express-promise-router')();
 const db = require('../db')
 
 router.get('/', async (req, res)=>{
-  if(!req.user)
-    return res.render('index')
+  if(!req.user){
+    const recents = await db.user.find().sort({ _id : -1 }).limit(6)
+    return res.render('index', {recents})
+  }
 
   const suggestions = await db.user.find({_id: {$ne: req.user._id, $not: {$in: req.user.blocked}}, interests: { $elemMatch: {$in: req.user.interests}}})
 
@@ -12,6 +14,7 @@ router.get('/', async (req, res)=>{
     var totalInterests = profile.interests.concat(req.user.interests).filter((v,i,self)=>self.indexOf(v) === i).length
     var matchedInterests = profile.interests.filter(v=>req.user.interests.indexOf(v) >= 0).length
     profile.compatibility = (matchedInterests / totalInterests * 100).toFixed(0)
+    if(isNaN(profile.compatibility)) profile.compatibility = 0
     return profile
   }).sort((a,b)=>b.compatibility-a.compatibility)
   
